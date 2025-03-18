6ecca7470fbfd7dd345d7bf0fae0d64722646332
Asad Ali <67062577+theasadali@users.noreply.github.com>
2025-03-18 20:17:02 +0500

    added form of creating posts

--- modernblog/blogapp/urls.py
+++ modernblog/blogapp/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.home, name="home"),
     path('post/<slug:slug>/', views.post_detail, name='post_detail'),
+    path('create/', views.create_post, name='create_post')
 ]
--- modernblog/blogapp/views.py
+++ modernblog/blogapp/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from .models import Post
+from .forms import PostForm
 
 def home(request):
     posts = Post.objects.all().order_by('published_at')
@@ -8,3 +9,16 @@ def home(request):
 def post_detail(request, slug):
     post = get_object_or_404(Post, slug=slug)
     return render(request, 'blog/post_detail.html', {'post': post})
+
+def create_post(request):
+    if request.method == "POST":
+        form = PostForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.save()
+            return redirect('home')
+    else:
+        form = PostForm()
+
+    return render(request, 'blog/create_post.html', {'form': form})
+    
